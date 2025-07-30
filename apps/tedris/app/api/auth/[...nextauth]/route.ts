294baa1eef1c6fb0c2b51a45c1ac63a6d7aa7057
Muhammed ALMAZ <mhmdalmz34@gmail.com>
2025-07-30 12:32:21 +0300

    .

--- apps/tedris/app/api/auth/[...nextauth]/route.ts
+++ apps/tedris/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,5 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import KeycloakProvider from 'next-auth/providers/keycloak';
-export const authOptions: AuthOptions = {
-  providers: [
-    KeycloakProvider({
-      clientId: process.env.KEYCLOAK_CLIENT_ID ?? '',
-      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET ?? '',
-      issuer: process.env.KEYCLOAK_ISSUER ?? '',
-      idToken: true,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.idToken = account.id_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken;
-      session.idToken = token.idToken as string;
-      return session;
-    },
-  },
-};
+import authOptions from 'apps/tedris/lib/auth_options';
+import NextAuth from 'next-auth';
+
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ apps/tedris/lib/auth_options.ts
@@ -0,0 +1,28 @@
+import { AuthOptions } from 'next-auth';
+import KeycloakProvider from 'next-auth/providers/keycloak';
+
+const authOptions: AuthOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID ?? '',
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET ?? '',
+      issuer: process.env.KEYCLOAK_ISSUER ?? '',
+      idToken: true,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.idToken = token.idToken as string;
+      return session;
+    },
+  },
+};
+export default authOptions;
