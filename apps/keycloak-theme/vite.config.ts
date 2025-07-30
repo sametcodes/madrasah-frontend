bf366f2e8d1d4c6cc0ce72c330b97d3af415cd93
Samet <hey@samet.codes>
2025-07-30 23:08:22 +0300

    feat: initialize keycloakify-starter with essential configurations and files

---
+++ apps/keycloak-theme/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { keycloakify } from "keycloakify/vite-plugin";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        react(),
+        keycloakify({
+            accountThemeImplementation: "none"
+        })
+    ]
+});
