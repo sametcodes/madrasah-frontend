185c3535980a937ae6437a58b63e296b5437cdd8
Samet <hey@samet.codes>
2025-08-12 21:49:43 +0300

    feat(tedris): update TypeScript configuration and add Jiti for environment imports

--- apps/tedris/next.config.js
+++ apps/tedris/next.config.js
@@ -1,6 +1,13 @@
+import { createJiti } from "jiti";
+const jiti = createJiti(import.meta.url, { debug: true });
+ 
+// Import env here to validate during build. Using jiti@^1 we can import .ts files :)
+jiti.import("./env.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@madrasah/ui', '@madrasah/utils', '@madrasah/types', '@madrasah/hooks'],
 };
 
-module.exports = nextConfig;
+
+export default nextConfig;
