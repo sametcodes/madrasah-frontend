74570b206499d574dcc872b40fe8db35a1de5501
Samet <hey@samet.codes>
2025-09-15 19:15:34 +0900

    chore(tedris): fixing linting issues

--- apps/tedris/lib/services.ts
+++ apps/tedris/lib/services.ts
@@ -1,16 +1,16 @@
-import 'server-only'; // Bu kodun istemciye gitmesini engeller
+import 'server-only'
 
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
-import { TedrisatService, createTedrisatClient } from '@madrasah/services/tedrisat';
-import { env } from '~/env';
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+import { TedrisatService, createTedrisatClient } from '@madrasah/services/tedrisat'
+import { env } from '~/env'
 
 export const getAuthenticatedApiService = (cookies: ReadonlyRequestCookies): TedrisatService => {
-  const token = cookies.get('next-auth.session-token')?.value;
+  const token = cookies.get('next-auth.session-token')?.value
 
   const client = createTedrisatClient({
     baseUrl: env.TEDRISAT_API_BASE_URL,
     token: token,
-  });
+  })
 
-  return new TedrisatService(client);
-};
+  return new TedrisatService(client)
+}
